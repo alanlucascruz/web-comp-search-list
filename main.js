515412860d5a1340b7b022f71add43c7cee0fb50
Alan Cruz <alanlucascruz@gmail.com>
2023-04-12 11:27:59 -0300

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+const onSearch = () => {
+  const input = document.querySelector("#search");
+  const filter = input.value.toUpperCase();
+
+  const list = document.querySelectorAll("#list li");
+
+  list.forEach((el) => {
+    const text = el.textContent.toUpperCase();
+
+    text.indexOf(filter) > -1
+      ? (el.style.display = "")
+      : (el.style.display = "none");
+  });
+};
